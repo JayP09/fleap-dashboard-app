d657240642694844c5890a305a6d17b1a765c754
Jay <panchalj272@gmail.com>
2023-03-03 00:08:05 +0530

    chore: Updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+// Importing necessary packages
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+// Importing components and context
+import Navbar from './components/Navbar/Navbar';
+import Dashboard from './pages/Dashboard/Dashboard';
+import { SidebarContextProvider } from './context/SidebarContext';
+
+// main component
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Fleap Dashboard</h1>
-    </div>
+    <SidebarContextProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </SidebarContextProvider>
   );
-}
+};
 
 export default App;
